10dad6488ea3a341f539a840551290223517a7c1
medukr <andrii.demydyuk@gmail.com>
2019-02-03 00:10:21 +0200

    Добавил анимацию запросов на странице тренровки, переделал вывод сообщения о ошибке там же

--- components/views/loadingModal.php
+++ components/views/loadingModal.php
@@ -1,12 +1,29 @@
 <!-- Loading Modal Begin-->
-<div class="modal" id="loadingModal" tabindex="-1" role="dialog" aria-labelledby="loadingModalLabel" aria-hidden="true">
-    <div class="modal-dialog  modal-dialog-centered modal-sm" role="document">
+<div class="modal fade" id="loadingModal" tabindex="-1" role="dialog" aria-labelledby="loadingModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog  modal-sm" role="document">
         <div class="cssload-container">
             <div class="cssload-speeding-wheel"></div>
         </div>
     </div>
 </div>
-
+<!-- Loading Modal END-->
+<!-- Error Modal Begin-->
+<div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-body p-3">
+                <div class="text-danger text-center">
+                    <i class="fa fa-times mx-2"></i>
+                    <strong id="errorText">Упс...Ошибочка!</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Error Modal END-->
+<!-- Loading Modal Style Begin-->
 <style>
     .cssload-container {
         width: 100%;
@@ -29,5 +46,4 @@
         100%{ transform: rotate(360deg); transform: rotate(360deg); }
     }
 </style>
-
-<!-- Loading Modal END-->
+<!-- Loading Modal Style END-->
